dc8b9a495b7f8827b9e5b4d6247d92427975098e
肉肉 <46618620+fbotp@users.noreply.github.com>
2023-11-29 15:53:13 +0800

    init

---
+++ shanxigongkaowang.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         shanxigongkaowang
+// @version      0.1
+// @author       You
+// @match        https://www.sxgkw.cn/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setTimeout(() => {
+        document.onselectstart = null;
+        document.oncontextmenu = null;
+        document.head.innerHTML += "<style>.zhaokao ul.list li {display: flex}\n .zhaokao ul.list li a {width: 93%;white-space: nowrap;text-overflow: ellipsis;overflow: hidden}</style>";
+        for(let i of document.querySelectorAll("body > div.proListClassify.clearfix > div > div > div.sectionC > div > div > div.info-cont p")) {
+            if (i.innerText.includes("扫码")) {
+                i.innerHTML = i.innerHTML.substr(0, i.innerHTML.indexOf("<br>"));
+            }
+            if (i.innerText.includes("来源")) {
+                i.innerHTML = i.innerHTML.substr(0, 3) + "<a href='" + i.innerHTML.substr(3) + "'>" + i.innerHTML.substr(3) + "</a>";
+            }
+        }
+        document.querySelectorAll("body > div.proListClassify.clearfix > div > div > div.sectionC > div > div > div.info-cont img").forEach((i) => i.remove())
+    }, 30)
+})();
