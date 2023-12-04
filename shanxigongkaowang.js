6815f8a26fd2ef88b7367d7ef9b1d8f7ad9063dd
肉肉 <46618620+fbotp@users.noreply.github.com>
2023-12-04 16:26:40 +0800

    modify a color

--- shanxigongkaowang.js
+++ shanxigongkaowang.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         shanxigongkaowang
 // @version      0.1
+// @description  try to take over the world!
 // @author       You
 // @match        https://www.sxgkw.cn/*
 // @grant        none
@@ -12,7 +13,7 @@
     setTimeout(() => {
         document.onselectstart = null;
         document.oncontextmenu = null;
-        document.head.innerHTML += "<style>.zhaokao ul.list li {display: flex}\n .zhaokao ul.list li a {width: 93%;white-space: nowrap;text-overflow: ellipsis;overflow: hidden}</style>";
+        document.head.innerHTML += "<style>.zhaokao ul.list li {display: flex}\n.zhaokao ul.list li a {width: 93%;white-space: nowrap;text-overflow: ellipsis;overflow: hidden}\na {color: revert}</style>";
         for(let i of document.querySelectorAll("body > div.proListClassify.clearfix > div > div > div.sectionC > div > div > div.info-cont p")) {
             if (i.innerText.includes("扫码")) {
                 i.innerHTML = i.innerHTML.substr(0, i.innerHTML.indexOf("<br>"));
